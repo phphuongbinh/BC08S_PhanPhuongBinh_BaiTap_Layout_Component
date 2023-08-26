c9d5f486a5bbe4c37e1b68267af412d0709cf80d
ErauQsPhan <mrsplol102@gmail.com>
2023-08-26 13:59:41 +0700

    upload code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import BaiTapLayouComponent from "./BaiTapLayouComponent/BaiTapLayouComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <BaiTapLayouComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/BaiTapLayouComponent/Item.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+export default class Item extends Component {
+  render() {
+    return (
+      <div className="col-lg-6 col-xxl-4 mb-5">
+        <div className="card bg-light border-0 h-100">
+          <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+            <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M6.75 7.5l3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0021 18V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v12a2.25 2.25 0 002.25 2.25z"
+                />
+              </svg>
+            </div>
+            <h2 className="fs-4 fw-bold">Fresh new layout</h2>
+            <p className="mb-0">
+              With Bootstrap 5, we've created a fresh new layout for this
+              template!
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
